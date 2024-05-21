414214c720d3166ae47091e215a7f069cb9a8f73
Aniket Patil <aniket.p@reapmind.com>
2024-05-21 11:03:37 +0530

    Initial Commit

---
+++ MapLocationUsingAPI/MapLocationUsingAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLocationUsingAPIApp.swift
+//  MapLocationUsingAPI
+//
+//  Created by Aniket Patil on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MapLocationUsingAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
